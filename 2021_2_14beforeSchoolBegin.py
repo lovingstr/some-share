547197e2bb829a654905cf252e8ebd380abf87c7
lovingstr <68695677+lovingstr@users.noreply.github.com>
2021-02-25 16:27:40 +0800

    Create 2021_2_14beforeSchoolBegin.py

---
+++ 2021_2_14beforeSchoolBegin.py
@@ -0,0 +1,23 @@
+from manimlib.imports import *
+from luoluoluo_useful.imports import *
+
+
+class First(FourierOfTexPaths):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE,
+        },
+
+        "n_vectors": 300,
+        "animated_name": "还有三天开学, 作业做完了吗",
+        "name_color": "#66CCFF",
+        "vector_config": {
+            "buff": 0,
+            "max_tip_length_to_length_ratio": 0.35,
+            "tip_length": 0.15,
+            "max_stroke_width_to_length_ratio": 10,
+            "stroke_width": 5,
+            "stroke_color": "#E2CCBC",
+            "stroke_opacity": .7,
+        },
+    }
